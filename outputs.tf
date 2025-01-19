f583f7b01b8cfa4a418f3c3f6d2dbb03284f2688
Moholkar@1998 <vaibhavmoholkar999@gmail.com>
2025-01-20 01:32:34 +0530

    Added VPC outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,20 @@
 #   value = module.jenkins.jenkins_url
 # }
 
+output "vpc_id" {
+  description = "The VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
 
 output "eks_cluster_role_arn" {
   value = module.eks-iam.eks_cluster_role_arn
